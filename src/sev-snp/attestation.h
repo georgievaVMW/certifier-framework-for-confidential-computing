26f4a680f04fcd32bc736a3f79c9daf32875c163
Aditya Gurajada <agurajada@vmware.com>
2023-08-15 11:55:43 -0700

    Clang-format fixes.

--- src/sev-snp/attestation.h
+++ src/sev-snp/attestation.h
@@ -6,12 +6,12 @@
 #ifndef MACOS
 #  include <linux/types.h>
 
-#ifndef uint64_t
+#  ifndef uint64_t
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned       uint32_t;
 typedef unsigned long  uint64_t;
-#endif
+#  endif
 
 #endif
 
--- src/sev-snp/snp_derive_key.h
+++ src/sev-snp/snp_derive_key.h
@@ -6,10 +6,10 @@
 #ifndef MACOS
 #  include <linux/types.h>
 #else
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned       uint32_t;
-typedef unsigned long long  uint64_t;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned           uint32_t;
+typedef unsigned long long uint64_t;
 #endif
 
 #define FIELD_POLICY_SHIFT      (0)
